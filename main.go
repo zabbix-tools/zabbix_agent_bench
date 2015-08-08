d398fde3ce3ee9d26b6f5eac1da4c70001040895
Ryan Armstrong <ryan@cavaliercoder.com>
2015-08-08 18:15:58 +0800

    Versioned for release 0.3.0

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ import (
 
 const (
 	APP         = "zabbix_agent_bench"
-	APP_VERSION = "0.2.0"
+	APP_VERSION = "0.3.0"
 	APP_AUTHOR  = "Ryan Armstrong <ryan@cavaliercoder.com>"
 
 	ZBX_NOTSUPPORTED = "ZBX_NOTSUPPORTED"
